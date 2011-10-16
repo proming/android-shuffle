0d626a6c9d64e2b9ce8755b8c5b0be525aae3629
Andy Bryant <andybryant@gmail.com>
2011-10-16 14:23:40 +0100

    move edit action changes

--- shuffle-app-engine/src/org/dodgybits/shuffle/gwt/core/EditActionView.java
+++ shuffle-app-engine/src/org/dodgybits/shuffle/gwt/core/EditActionView.java
@@ -1,9 +1,14 @@
 package org.dodgybits.shuffle.gwt.core;
 
-import com.gwtplatform.mvp.client.ViewImpl;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.datepicker.client.DateBox;
 import com.google.inject.Inject;
+import com.gwtplatform.mvp.client.ViewImpl;
 
 public class EditActionView extends ViewImpl implements EditActionPresenter.MyView {
 
@@ -12,6 +17,26 @@ public class EditActionView extends ViewImpl implements EditActionPresenter.MyVi
 	public interface Binder extends UiBinder<Widget, EditActionView> {
 	}
 
+	@UiField Button save;
+	
+	@UiField
+	TextBox description;
+
+	@UiField
+	TextBox context;
+
+	@UiField
+	TextBox project;
+
+	@UiField
+	TextArea details;
+
+	@UiField
+	DateBox from;
+
+	@UiField
+	DateBox due;
+	
 	@Inject
 	public EditActionView(final Binder binder) {
 		widget = binder.createAndBindUi(this);
