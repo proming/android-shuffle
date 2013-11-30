b3035e01182179bb267a2c676382a201e5be13b9
Andy Bryant <andybryant@gmail.com>
2013-11-30 21:18:44 +1100

    Got code compiling and running

--- ShuffleAndroid/src/main/java/org/dodgybits/shuffle/android/server/gcm/GCMIntentService.java
+++ ShuffleAndroid/src/main/java/org/dodgybits/shuffle/android/server/gcm/GCMIntentService.java
@@ -21,8 +21,8 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
-import com.google.android.gcm.GCMBaseIntentService;
-import com.google.android.gcm.GCMRegistrar;
+//import com.google.android.gcm.GCMBaseIntentService;
+//import com.google.android.gcm.GCMRegistrar;
 import org.dodgybits.android.shuffle.R;
 import org.dodgybits.shuffle.android.preference.activity.PreferencesAppEngineSynchronizationActivity;
 
@@ -31,74 +31,74 @@ import static org.dodgybits.shuffle.android.server.gcm.CommonUtilities.SENDER_ID
 /**
  * IntentService responsible for handling GCM messages.
  */
-public class GCMIntentService extends GCMBaseIntentService {
+public class GCMIntentService { //} extends GCMBaseIntentService {
 
-    @SuppressWarnings("hiding")
-    private static final String TAG = "GCMIntentService";
-
-    public GCMIntentService() {
-        super(SENDER_ID);
-    }
-
-    @Override
-    protected void onRegistered(Context context, String registrationId) {
-        Log.i(TAG, "Device registered: regId = " + registrationId);
-        ServerUtilities.register(context, registrationId);
-    }
-
-    @Override
-    protected void onUnregistered(Context context, String registrationId) {
-        Log.i(TAG, "Device unregistered");
-        if (GCMRegistrar.isRegisteredOnServer(context)) {
-            ServerUtilities.unregister(context, registrationId);
-        } else {
-            // This callback results from the call to unregister made on
-            // ServerUtilities when the registration to the server failed.
-            Log.i(TAG, "Ignoring unregister callback");
-        }
-    }
-
-    @Override
-    protected void onMessage(Context context, Intent intent) {
-        Log.i(TAG, "Received message");
-    }
-
-    @Override
-    protected void onDeletedMessages(Context context, int total) {
-        Log.i(TAG, "Received deleted messages notification");
-    }
-
-    @Override
-    public void onError(Context context, String errorId) {
-        Log.i(TAG, "Received error: " + errorId);
-    }
-
-    @Override
-    protected boolean onRecoverableError(Context context, String errorId) {
-        // log message
-        Log.i(TAG, "Received recoverable error: " + errorId);
-        return super.onRecoverableError(context, errorId);
-    }
-
-    /**
-     * Issues a notification to inform the user that server has sent a message.
-     */
-    private static void generateNotification(Context context, String message) {
-        int icon = R.drawable.shuffle_icon;
-        long when = System.currentTimeMillis();
-        NotificationManager notificationManager = (NotificationManager)
-                context.getSystemService(Context.NOTIFICATION_SERVICE);
-        Notification notification = new Notification(icon, message, when);
-        String title = context.getString(R.string.app_name);
-        Intent notificationIntent = new Intent(context, PreferencesAppEngineSynchronizationActivity.class);
-        // set intent so it does not start a new activity
-        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
-                Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        PendingIntent intent =
-                PendingIntent.getActivity(context, 0, notificationIntent, 0);
-        notification.setLatestEventInfo(context, title, message, intent);
-        notification.flags |= Notification.FLAG_AUTO_CANCEL;
-        notificationManager.notify(0, notification);
-    }
+//    @SuppressWarnings("hiding")
+//    private static final String TAG = "GCMIntentService";
+//
+//    public GCMIntentService() {
+//        super(SENDER_ID);
+//    }
+//
+//    @Override
+//    protected void onRegistered(Context context, String registrationId) {
+//        Log.i(TAG, "Device registered: regId = " + registrationId);
+//        ServerUtilities.register(context, registrationId);
+//    }
+//
+//    @Override
+//    protected void onUnregistered(Context context, String registrationId) {
+//        Log.i(TAG, "Device unregistered");
+//        if (GCMRegistrar.isRegisteredOnServer(context)) {
+//            ServerUtilities.unregister(context, registrationId);
+//        } else {
+//            // This callback results from the call to unregister made on
+//            // ServerUtilities when the registration to the server failed.
+//            Log.i(TAG, "Ignoring unregister callback");
+//        }
+//    }
+//
+//    @Override
+//    protected void onMessage(Context context, Intent intent) {
+//        Log.i(TAG, "Received message");
+//    }
+//
+//    @Override
+//    protected void onDeletedMessages(Context context, int total) {
+//        Log.i(TAG, "Received deleted messages notification");
+//    }
+//
+//    @Override
+//    public void onError(Context context, String errorId) {
+//        Log.i(TAG, "Received error: " + errorId);
+//    }
+//
+//    @Override
+//    protected boolean onRecoverableError(Context context, String errorId) {
+//        // log message
+//        Log.i(TAG, "Received recoverable error: " + errorId);
+//        return super.onRecoverableError(context, errorId);
+//    }
+//
+//    /**
+//     * Issues a notification to inform the user that server has sent a message.
+//     */
+//    private static void generateNotification(Context context, String message) {
+//        int icon = R.drawable.shuffle_icon;
+//        long when = System.currentTimeMillis();
+//        NotificationManager notificationManager = (NotificationManager)
+//                context.getSystemService(Context.NOTIFICATION_SERVICE);
+//        Notification notification = new Notification(icon, message, when);
+//        String title = context.getString(R.string.app_name);
+//        Intent notificationIntent = new Intent(context, PreferencesAppEngineSynchronizationActivity.class);
+//        // set intent so it does not start a new activity
+//        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+//                Intent.FLAG_ACTIVITY_SINGLE_TOP);
+//        PendingIntent intent =
+//                PendingIntent.getActivity(context, 0, notificationIntent, 0);
+//        notification.setLatestEventInfo(context, title, message, intent);
+//        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+//        notificationManager.notify(0, notification);
+//    }
 
 }
