5686ba957070f2659019720dd484fe0cd362b0e4
Andy Bryant <andybryant@gmail.com>
2014-01-09 23:35:26 +1100

    Use concourse for nav drawer

--- ShuffleAndroid/src/main/java/org/dodgybits/shuffle/android/core/view/IconNameCountListItem.java
+++ ShuffleAndroid/src/main/java/org/dodgybits/shuffle/android/core/view/IconNameCountListItem.java
@@ -1,10 +1,12 @@
 package org.dodgybits.shuffle.android.core.view;
 
 import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import org.dodgybits.android.shuffle.R;
+import org.dodgybits.shuffle.android.core.util.FontUtils;
 
 public class IconNameCountListItem extends LinearLayout {
     private ImageView mIcon;
@@ -19,8 +21,9 @@ public class IconNameCountListItem extends LinearLayout {
     public void init(android.content.Context androidContext) {
         LayoutInflater vi = (LayoutInflater)androidContext.
                 getSystemService(android.content.Context.LAYOUT_INFLATER_SERVICE);
-        vi.inflate(R.layout.icon_count_list_view, this, true);
+        ViewGroup view = (ViewGroup) vi.inflate(R.layout.icon_count_list_view, this, true);
 
+        FontUtils.setCustomFont(view, androidContext.getAssets());
         mName = (TextView) findViewById(R.id.name);
         mCount = (TextView) findViewById(R.id.count);
         mIcon = (ImageView) findViewById(R.id.icon);
